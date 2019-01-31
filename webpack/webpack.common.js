634618b237d95a8d6cb88a0e32d8095edcdb7631
maxinakenty <mgreen097@gmail.com>
2019-01-31 20:01:57 +0300

    build(webpack): add webpack

---
+++ webpack/webpack.common.js
@@ -0,0 +1,40 @@
+const { NoEmitOnErrorsPlugin } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { resolve } = require('path');
+const { SRC } = require('./constants');
+
+module.exports = {
+  context: SRC,
+  entry: {
+    bundle: ['@babel/polyfill', './index'],
+  },
+  resolve: {
+    modules: ['node_modules'],
+    alias: {
+      '@styles': resolve(SRC, 'styles'),
+      '@api': resolve(SRC, 'api'),
+    },
+  },
+  plugins: [
+    new NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Index',
+      template: resolve(SRC, 'index.html'),
+      filename: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.js$/,
+        use: 'source-map-loader',
+        enforce: 'pre',
+      },
+    ],
+  },
+};
---
+++ webpack/webpack.development.js
@@ -0,0 +1,111 @@
+const { DefinePlugin, HotModuleReplacementPlugin } = require('webpack');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+const HtmlWebpackReloadPlugin = require('html-webpack-reload-plugin');
+const { FAVICON, POSTCSS, HTML } = require('./constants');
+
+module.exports = {
+  mode: 'development',
+  output: {
+    publicPath: '/',
+    filename: '[name].js',
+    chunkFilename: '[name].js',
+  },
+  devtool: 'eval',
+  watch: true,
+  watchOptions: {
+    aggregateTimeout: 100,
+  },
+  devServer: {
+    overlay: true,
+    port: 3000,
+    hot: true,
+    stats: {
+      'errors-only': true,
+    },
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackReloadPlugin(),
+    new DefinePlugin({
+      NODE_ENV: JSON.stringify('development'),
+    }),
+    new HotModuleReplacementPlugin(),
+    new FaviconsWebpackPlugin({
+      logo: FAVICON,
+      prefix: 'icons/',
+      emitStats: false,
+      statsFilename: 'iconstats.json',
+      background: '#fff',
+      persistentCache: true,
+      inject: true,
+      icons: {
+        android: true,
+        appleIcon: true,
+        appleStartup: true,
+        coast: false,
+        favicons: true,
+        firefox: true,
+        opengraph: false,
+        twitter: false,
+        yandex: false,
+        windows: false,
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /js/,
+        enforce: 'pre',
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'eslint-loader',
+            options: {
+              fix: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif|svg|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          'resolve-url-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: POSTCSS,
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
